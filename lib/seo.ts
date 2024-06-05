a91db5350c5917d6326ff610e775a09c76ddb85b
DabAZ <dabaz@dabaz.me>
2024-06-06 07:51:45 +0800

    chore: add metadata

---
+++ lib/seo.ts
@@ -0,0 +1,13 @@
+export const seo = {
+  home: {
+    title: "DabNotion - Premium Notion Templates",
+    description: "A collection of premium Notion templates to boost productivity, organize projects, and enhance personal planning.",
+    url: new URL(
+      process.env.NODE_ENV === "production"
+        ? "https://notion.dabaz.me"
+        : "http://localhost:3000"
+    ),
+    imageSrc: "https://notion.dabaz.me/images/og.jpg",
+    imageAlt: "DabNotion logo with 'DabNotion' on a white background.",
+    siteName: "DabNotion",
+  }} as const;
