4ebbfc8710119bcd7e5727f9277f312da16ada88
DabAZ <dabaz@dabaz.me>
2024-06-07 18:10:08 +0800

    feat: add other products

--- components/store/ProductList.tsx
+++ components/store/ProductList.tsx
@@ -11,7 +11,17 @@ const products = {
       price: '$389',
       discount: '%61',
       imageSrc: '/images/products/everything-bundle.png',
-    }
+    },
+    {
+      id: 2,
+      name: 'Resolution Bundle',
+      tags: ['Personal'],
+      href: 'https://gumroad.com/a/429277395/YkJkr',
+      ogPrice: '$54',
+      price: '$19',
+      discount: '%65',
+      imageSrc: '/images/products/resolution-bundle.png',
+    },
   ],
   personal: [
     {
@@ -41,14 +51,48 @@ const products = {
       imageSrc: '/images/products/finance-tracker.png',
     },
   ],
-  business: [
+  pro: [
     {
       id: 1,
+      name: 'Notion Second Brain',
+      tags: ['Personal', 'Project Management'],
+      href: 'https://gumroad.com/a/429277395/qwzxl',
+      price: 'From $79',
+      imageSrc: '/images/products/second-brain.png',
+    },
+    {
+      id: 2,
       name: 'Freelance OS',
       tags: ['Personal', 'Professional', 'Business'],
       href: 'https://gumroad.com/a/429277395/gpckg',
       price: 'From $79',
       imageSrc: '/images/products/freelance-os.jpeg',
+    },
+    {
+      id: 3,
+      name: 'Content OS',
+      tags: ['Personal', 'Professional', 'Business', 'Content'],
+      href: 'https://gumroad.com/a/429277395/vhdyw',
+      price: 'From $49',
+      imageSrc: '/images/products/content-os.png',
+    },
+    {
+      id: 4,
+      name: 'Content Calendar',
+      tags: ['Personal', 'Professional', 'Business', 'Content'],
+      href: 'https://gumroad.com/a/429277395/wxtvs',
+      price: 'From $0',
+      imageSrc: '/images/products/content-calendar.png',
+    },
+  ],
+  course: [
+    {
+      id: 1,
+      name: 'Notion Creator Course',
+      tags: ['Professional', 'Business', 'Course'],
+      href: 'https://gumroad.com/a/429277395/unndvl',
+      price: '$149',
+      imageSrc: '/images/products/notion-creator-course.webp',
     },
   ]
 }
@@ -139,6 +183,86 @@ export function ProductList() {
                     {product.ogPrice}
                   </s>
                 </span>
+                {product.price}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-16 flex items-center justify-between space-x-4">
+        <h2 className="text-lg font-semibold text-black">
+          Professional
+        </h2>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
+        {products.pro.map((product) => (
+          <div key={product.id} className="group relative">
+            <div className="aspect-h-3 aspect-w-6 overflow-hidden rounded-lg bg-neutral-100">
+              <Image
+                width={600}
+                height={315}
+                src={product.imageSrc}
+                alt={product.name} 
+                className="object-cover object-center"
+              />
+              <div className="flex items-end p-2 opacity-0 group-hover:opacity-100" aria-hidden="true">
+                <div className="w-full rounded-md bg-white bg-opacity-75 px-4 py-2 text-center text-sm font-medium text-black backdrop-blur-sm backdrop-filter">
+                  View Product
+                </div>
+              </div>
+            </div>
+            <div className="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-black">
+              <h3>
+                <a
+                  href={product.href}
+                  target="_blank"
+                >
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  {product.name}
+                </a>
+              </h3>
+              <p className="flex gap-1">
+                {product.price}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-16 flex items-center justify-between space-x-4">
+        <h2 className="text-lg font-semibold text-black">
+          Course
+        </h2>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
+        {products.course.map((product) => (
+          <div key={product.id} className="group relative">
+            <div className="aspect-h-3 aspect-w-6 overflow-hidden rounded-lg bg-neutral-100">
+              <Image
+                width={600}
+                height={315}
+                src={product.imageSrc}
+                alt={product.name} 
+                className="object-cover object-center"
+              />
+              <div className="flex items-end p-2 opacity-0 group-hover:opacity-100" aria-hidden="true">
+                <div className="w-full rounded-md bg-white bg-opacity-75 px-4 py-2 text-center text-sm font-medium text-black backdrop-blur-sm backdrop-filter">
+                  View Product
+                </div>
+              </div>
+            </div>
+            <div className="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-black">
+              <h3>
+                <a
+                  href={product.href}
+                  target="_blank"
+                >
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  {product.name}
+                </a>
+              </h3>
+              <p className="flex gap-1">
                 {product.price}
               </p>
             </div>
