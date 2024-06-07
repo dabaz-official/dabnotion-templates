fb21ba17b44e1ed1d0c95c2f80946aacdf123ff2
DabAZ <dabaz@dabaz.me>
2024-06-07 18:13:45 +0800

    chore: update footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,3 +1,5 @@
+import { Logo } from "@dabaz/components/common/Logo";
+
 const today = new Date();
 const year = today.getFullYear();
 
@@ -5,8 +7,9 @@ export function Footer() {
   return (
     <footer className="bg-white">
       <div className="mx-auto max-w-7xl px-6 py-8 md:flex md:items-center md:justify-center lg:px-8">
-        <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-center text-sm leading-5 text-neutral-600">
+        <div className="md:order-1">
+          <Logo className="h-8 md:h-10 w-auto mx-auto" />
+          <p className="text-center text-sm leading-5 text-neutral-600 mt-4">
             &copy; {year} DabAZ. All rights reserved.
           </p>
         </div>
